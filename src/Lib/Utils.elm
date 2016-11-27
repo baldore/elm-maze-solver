7d0a309bee23e5c9c66f3ab994be54b76ab1aed0
baldore <acornssn@gmail.com>
2016-11-27 10:41:55 -0500

    Working in a get for lists

---
+++ src/Lib/Utils.elm
@@ -0,0 +1,13 @@
+-- module Lib.Utils exposing (..)
+
+
+-- getFromList : Int -> List a -> Maybe a
+-- getFromList index list =
+--     if List.length list <= index || index < 0 then
+--         Nothing
+--     else
+--         case ( list, index ) of
+--             ( [], _ ) ->
+--                 Nothing
+
+--             ( [], _ ) ->
--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,9 @@ import Html.Attributes as Attr
 import Html.Events exposing (onInput, onClick)
 
 
+-- import Lib.Utils exposing (getFromList)
+
+
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
@@ -22,9 +25,21 @@ type alias Cols =
     Int
 
 
+type Cell
+    = Cell
+        { row : Int
+        , col : Int
+        }
+
+
+type alias Grid =
+    List (List Cell)
+
+
 type alias Model =
     { rows : Rows
     , cols : Cols
+    , grid : Grid
     }
 
 
@@ -32,41 +47,61 @@ model : Model
 model =
     { rows = 0
     , cols = 0
+    , grid = []
     }
 
 
 type Msg
     = UpdateRows String
     | UpdateCols String
-    | SelectCell Int Int
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         UpdateRows value ->
-            { model | rows = Result.withDefault 0 (String.toInt value) }
+            { model
+                | rows =
+                    Result.withDefault 0 (String.toInt value)
+                    -- , grid = updateGrid model.grid model.rows model.cols
+            }
 
         UpdateCols value ->
-            { model | cols = Result.withDefault 0 (String.toInt value) }
+            { model
+                | cols =
+                    Result.withDefault 0 (String.toInt value)
+                    -- , grid = updateGrid model.grid model.rows model.cols
+            }
 
-        _ ->
-            model
 
 
-getTable : Rows -> Cols -> Html Msg
-getTable rows cols =
-    let
-        trs =
-            List.range 0 (rows - 1) |> List.map (\row -> tr [] (makeTds row))
-
-        makeTds row =
-            List.range 0 (cols - 1) |> List.map (makeTd row)
-
-        makeTd row col =
-            td [] [ button [] [ text (toString ( row, col )) ] ]
-    in
-        table [] trs
+-- updateGrid : Grid -> Rows -> Cols -> Grid
+-- updateGrid grid rows cols =
+--     case ( grid, rows, cols ) of
+--         ( [], _, _ ) ->
+--             []
+--         ( _, 0, _ ) ->
+--             []
+--         ( _, _, _ ) ->
+--             []
+--         ( grid, _, _ ) ->
+--             grid
+--         ( _, _, _ ) ->
+--             []
+-- updateGrid grid rows cols =
+-- _ ->
+--     model
+-- getTable : Grid -> Html Msg
+-- getTable grid =
+-- let
+--     trs =
+--         List.range 0 (rows - 1) |> List.map (\row -> tr [] (makeTds row))
+--     makeTds row =
+--         List.range 0 (cols - 1) |> List.map (makeTd row)
+--     makeTd row col =
+--         td [] [ button [] [ text (toString ( row, col )) ] ]
+-- in
+--     table [] trs
 
 
 view : Model -> Html Msg
@@ -99,5 +134,5 @@ view model =
                 ]
             ]
         , h2 [] [ text "Result" ]
-        , getTable model.rows model.cols
+          -- , getTable model.grid
         ]
