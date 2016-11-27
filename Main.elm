9e5fc4cae34cd47a60ead0150fe18e840d4b1d74
baldore <acornssn@gmail.com>
2016-11-26 22:10:18 -0500

    Add basic counter example

--- Main.elm
+++ Main.elm
@@ -1,7 +1,43 @@
 module Main exposing (..)
 
-import Html exposing (..)
+import Html exposing (Html, text, div, button)
+import Html.Events exposing (onClick)
 
 
+main : Program Never Model Msg
 main =
-    text "hola mundo genial"
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+model : Model
+model =
+    { counter = 0
+    }
+
+
+type Msg
+    = Increment
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | counter = model.counter + 1 }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text ("hola mundo genial: " ++ toString (model.counter)) ]
+        , button [ onClick Increment ] [ text "Click me" ]
+        ]
